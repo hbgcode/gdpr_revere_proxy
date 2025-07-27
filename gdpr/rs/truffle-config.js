bb99c3144188b34d5dc7a7a72ceadd5739ce10a2
megh-git-alt <meghaduttaone@gmail.com>
2025-07-27 16:40:49 +0530

    Update truffle-config.js

--- gdpr/rs/truffle-config.js
+++ gdpr/rs/truffle-config.js
@@ -6,7 +6,8 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "/build"),
   networks: {
     development: {
-      host: "127.0.0.1",
+      //host: "127.0.0.1",
+      host: "103.77.138.152",
       port: 7545,
       network_id: "5777" //Match any network id
     }
